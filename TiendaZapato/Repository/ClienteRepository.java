60f71f049056714f05641bec782e1820697ee0fd
DeyvidtSalvatierra <Deyvidt17@gmail.com>
2026-09-02 22:22:32 -0500

    Go

---
+++ TiendaZapato/Repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.tiendazapatos.repository;
+
+import com.tiendazapatos.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
---
+++ TiendaZapato/TiendaZapatosApplication.java
@@ -0,0 +1,12 @@
+package com.tiendazapatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaZapatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaZapatosApplication.class, args);
+    }
+}
