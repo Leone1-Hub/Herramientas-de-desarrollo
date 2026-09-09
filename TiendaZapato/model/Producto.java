98da39361de966aa6e17f35e8b1445483d5691c8
Sp4s3r <sergestudents97@gmail.com>
2026-09-09 17:41:23 -0500

    Modelo Producto completo con constructores y metodos

--- TiendaZapato/model/Producto.java
+++ TiendaZapato/model/Producto.java
@@ -10,4 +10,58 @@ public class Producto {
     private String categoria;
     private Double precio;
     private int stock;
+
+    public Producto(){
+    }
+
+    public Producto(String nombre, String categoria, Double precio, int stock){
+        this.nombre = nombre;  //atributo y parametro
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+
+    public Double getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(Double precio){
+        this.precio = precio;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
 }
+
+
+
