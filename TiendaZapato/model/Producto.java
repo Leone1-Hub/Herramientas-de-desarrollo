1c89b3ee433351684b9aa735b90f6d815d32f264
Sp4s3r <sergestudents97@gmail.com>
2026-09-09 05:09:05 -0500

    Agregue atributos del modelo Producto

--- TiendaZapato/model/Producto.java
+++ TiendaZapato/model/Producto.java
@@ -1,5 +1,13 @@
 package com.tiendazapatos.model;
 
+@Entity
 public class Producto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String categoria;
+    private Double precio;
+    private int stock;
 }
